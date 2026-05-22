f7e580493d9b2048420b41be6c6fbc6e763212a2
MoonEunSeo <mos8594@naver.com>
2026-05-22 14:20:32 +0900

    refactor: extract vue router setup

---
+++ src/app/router.ts
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import FeedView from '../views/FeedView.vue'
+import RecipeDetailView from '../views/RecipeDetailView.vue'
+import RecipeEditorView from '../views/RecipeEditorView.vue'
+import SavedView from '../views/SavedView.vue'
+import MyPageView from '../views/MyPageView.vue'
+import AuthView from '../views/AuthView.vue'
+import RecommendationsView from '../views/RecommendationsView.vue'
+import IngredientsView from '../views/IngredientsView.vue'
+import BackendApiView from '../views/BackendApiView.vue'
+import ServerErrorView from '../views/ServerErrorView.vue'
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: HomeView },
+    { path: '/feed', component: FeedView },
+    { path: '/recipe/:id', component: RecipeDetailView },
+    { path: '/my-recipe/new', component: RecipeEditorView },
+    { path: '/saved', component: SavedView },
+    { path: '/mypage', component: MyPageView },
+    { path: '/login', component: AuthView, props: { mode: 'login' } },
+    { path: '/oauth/callback', component: AuthView, props: { mode: 'login' } },
+    { path: '/signup', component: AuthView, props: { mode: 'signup' } },
+    { path: '/recommendations', component: RecommendationsView },
+    { path: '/ingredients', component: IngredientsView },
+    { path: '/backend-api', component: BackendApiView },
+    { path: '/server-error', component: ServerErrorView },
+  ],
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import HomeView from './views/HomeView.vue'
-import FeedView from './views/FeedView.vue'
-import RecipeDetailView from './views/RecipeDetailView.vue'
-import RecipeEditorView from './views/RecipeEditorView.vue'
-import SavedView from './views/SavedView.vue'
-import MyPageView from './views/MyPageView.vue'
-import AuthView from './views/AuthView.vue'
-import RecommendationsView from './views/RecommendationsView.vue'
-import IngredientsView from './views/IngredientsView.vue'
-import BackendApiView from './views/BackendApiView.vue'
-import ServerErrorView from './views/ServerErrorView.vue'
+import { router } from './app/router'
 import { fallbackImage } from './api'
 import './styles.css'
 if (typeof window !== 'undefined') {
@@ -31,27 +20,5 @@ if (typeof window !== 'undefined') {
     true,
   )
 }
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomeView },
-    { path: '/feed', component: FeedView },
-    { path: '/recipe/:id', component: RecipeDetailView },
-    { path: '/my-recipe/new', component: RecipeEditorView },
-    { path: '/saved', component: SavedView },
-    { path: '/mypage', component: MyPageView },
-    { path: '/login', component: AuthView, props: { mode: 'login' } },
-    { path: '/oauth/callback', component: AuthView, props: { mode: 'login' } },
-    { path: '/signup', component: AuthView, props: { mode: 'signup' } },
-    { path: '/recommendations', component: RecommendationsView },
-    { path: '/ingredients', component: IngredientsView },
-    { path: '/backend-api', component: BackendApiView },
-    { path: '/server-error', component: ServerErrorView },
-  ],
-  scrollBehavior() {
-    return { top: 0 }
-  },
-})
 
 createApp(App).use(router).mount('#app')
